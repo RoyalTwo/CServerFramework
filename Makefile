52d7e1de41f1fd5399c19868b8d0df6408f0edd1
Logan Kissel <kissellogan10@gmail.com>
2024-03-05 11:58:41 -0500

    Rearranged files, reworked Makefile

--- Makefile
+++ Makefile
@@ -4,9 +4,13 @@ COMPILER := clang
 LIB_DIR := lib
 
 build: 
+	$(COMPILER) -c $(SRC_DIR)/main.c -I$(LIB_DIR)/ -o $(BIN_DIR)/objs/main.o
+	ar rcs $(BIN_DIR)/libserver.a $(BIN_DIR)/objs/main.o
 
 run-example: $(SRC_DIR)/example.out
-	./$(BIN_DIR)/example.out
+	./example/example.out
 
 $(SRC_DIR)/example.out:
-	$(COMPILER) example/example.c $(SRC_DIR)/main.c -I$(LIB_DIR)/ -o $(BIN_DIR)/example.out
+	cp $(BIN_DIR)/libserver.a $(LIB_DIR)/server.h example/
+	$(COMPILER) example/libserver.a example/example.c -Iexample/ -o example/example.out
+
---
+++ example/server.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+typedef struct
+{
+    char path[256];
+    int _client_fd;
+} Request;
+
+typedef struct
+{
+    void (*sendFile)(Request, char *);
+    void (*sendString)(Request, char *);
+} Response;
+
+// Response should have sendFile() and sendString()
+
+struct get_handler
+{
+    void (*func)(Request, Response);
+    // TODO: change from 256
+    char path[256];
+};
+
+struct server_config
+{
+    void (*not_found_handler)(Request);
+};
+
+typedef struct
+{
+    struct server_config config;
+    int _listenfd;
+    struct sockaddr_in _addr;
+    struct get_handler _get_handlers[256];
+    int _get_count;
+} server_t;
+
+typedef enum
+{
+    HTTP_GET,
+    HTTP_POST,
+    HTTP_NONE
+} HTTP_TYPE;
+
+server_t CreateServer(struct server_config conf);
+void ServerListen(server_t *app, int port);
+void ServerRegister(server_t *app, char *path, HTTP_TYPE type, void (*handler)(Request, Response));
+void ServerRun(server_t *app);
